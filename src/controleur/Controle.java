08b99f013bfb649ae7bb3c068d2f5819211f1d57
iyedk <iyedk@192.168.1.161>
2025-05-27 18:15:45 +0200

    mise a jour

---
+++ src/controleur/Controle.java
@@ -0,0 +1,179 @@
+package controleur;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import modele.Jeu;
+import modele.JeuClient;
+import modele.JeuServeur;
+import outils.connexion.AsyncResponse;
+import outils.connexion.ClientSocket;
+import outils.connexion.Connection;
+import outils.connexion.ServeurSocket;
+import vue.Arene;
+import vue.ChoixJoueur;
+import vue.EntreeJeu;
+
+/**
+ * Contrôleur et point d'entrée de l'applicaton 
+ * @author emds
+ *
+ */
+public class Controle implements AsyncResponse, Global {
+
+	/**
+	 * frame EntreeJeu
+	 */
+	private EntreeJeu frmEntreeJeu ;
+	/**
+	 * frame Arene
+	 */
+	private Arene frmArene;
+	/**
+	 * frame ChoixJoueur
+	 */
+	private ChoixJoueur frmChoixJoueur;
+	/**
+	 * instance du jeu (JeuServeur ou JeuClient)
+	 */
+	private Jeu leJeu;
+
+	/**
+	 * Méthode de démarrage
+	 * @param args non utilisé
+	 */
+	public static void main(String[] args) {
+		new Controle();
+	}
+	
+	/**
+	 * Constructeur
+	 */
+	private Controle() {
+		this.frmEntreeJeu = new EntreeJeu(this) ;
+		this.frmEntreeJeu.setVisible(true);
+	}
+	
+	/**
+	 * Demande provenant de la vue EntreeJeu
+	 * @param info information à traiter
+	 */
+	public void evenementEntreeJeu(String info) {
+		if(info.equals(SERVEUR)) {
+			new ServeurSocket(this, PORT);
+			this.leJeu = new JeuServeur(this);
+			this.frmEntreeJeu.dispose();
+			this.frmArene = new Arene(this, SERVEUR);
+			((JeuServeur)this.leJeu).constructionMurs();
+			this.frmArene.setVisible(true);
+		} else {
+			new ClientSocket(this, info, PORT);
+		}
+	}
+	
+	/**
+	 * Informations provenant de la vue ChoixJoueur
+	 * @param pseudo le pseudo du joueur
+	 * @param numPerso le numéro du personnage choisi par le joueur
+	 */
+	public void evenementChoixJoueur(String pseudo, int numPerso) {
+		this.frmChoixJoueur.dispose();
+		this.frmArene.setVisible(true);
+		((JeuClient)this.leJeu).envoi(PSEUDO+STRINGSEPARE+pseudo+STRINGSEPARE+numPerso);
+	}
+	
+	/**
+	 * Information provenant de la vue Arene
+	 * @param info information à transférer
+	 */
+	public void evenementArene(Object info) {
+		if(info instanceof String) {
+			((JeuClient)this.leJeu).envoi(TCHAT+STRINGSEPARE+info);
+		}else if (info instanceof Integer) {
+			((JeuClient)this.leJeu).envoi(ACTION+STRINGSEPARE+info);
+		}
+	}
+	
+	/**
+	 * Demande provenant de JeuServeur
+	 * @param ordre ordre à exécuter
+	 * @param info information à traiter
+	 */
+	public void evenementJeuServeur(String ordre, Object info) {
+		switch(ordre) {
+		case AJOUTMUR :
+			frmArene.ajoutMurs(info);
+			break;
+		case AJOUTPANELMURS :
+			this.leJeu.envoi((Connection)info, this.frmArene.getJpnMurs());
+			break;
+		case AJOUTJLABELJEU :
+			this.frmArene.ajoutJLabelJeu((JLabel)info);
+			break;
+		case MODIFPANELJEU :
+			this.leJeu.envoi((Connection)info, this.frmArene.getJpnJeu());
+			break;
+		case AJOUTPHRASE :
+			this.frmArene.ajoutTchat((String)info);
+			((JeuServeur)this.leJeu).envoi(this.frmArene.getTxtChat());
+			break;
+		}
+	}
+	
+	/**
+	 * Demande provenant de JeuClient
+	 * @param ordre ordre à exécuter
+	 * @param info information à traiter
+	 */
+	public void evenementJeuClient(String ordre, Object info) {
+		switch(ordre) {
+		case AJOUTPANELMURS :
+			this.frmArene.setJpnMurs((JPanel)info);
+			break;
+		case MODIFPANELJEU :
+			this.frmArene.setJpnJeu((JPanel)info);
+			break;
+		case MODIFTCHAT :
+			this.frmArene.setTxtChat((String)info);
+			break;
+		case JOUESON :
+			this.frmArene.joueSon((Integer)info);
+			break;
+		}
+	}
+
+	/**
+	 * Envoi d'informations vers l'ordinateur distant
+	 * @param connection objet de connexion pour l'envoi vers l'ordinateur distant
+	 * @param info information à envoyer
+	 */
+	public void envoi(Connection connection, Object info) {
+		connection.envoi(info);
+	}
+	
+	@Override
+	public void reception(Connection connection, String ordre, Object info) {
+		switch(ordre) {
+		case CONNEXION :
+			if(!(this.leJeu instanceof JeuServeur)) {
+				this.leJeu = new JeuClient(this);
+				this.leJeu.connexion(connection);
+				this.frmEntreeJeu.dispose();
+				this.frmArene = new Arene(this, CLIENT);
+				this.frmChoixJoueur = new ChoixJoueur(this);
+				this.frmChoixJoueur.setVisible(true);
+			} else {
+				this.leJeu.connexion(connection);
+			}
+			break;
+		case RECEPTION :
+			this.leJeu.reception(connection, info);
+			break;
+		case DECONNEXION :
+			this.leJeu.deconnexion(connection);
+			break;
+		}
+		
+	}
+
+}
---
+++ src/controleur/Global.java
@@ -0,0 +1,266 @@
+/**
+ * 
+ */
+package controleur;
+
+/**
+ * Global contient les constantes du programme
+ * @author emds
+ *
+ */
+public interface Global {
+	
+	/**
+	 * N° du port d'écoute du serveur
+	 */
+	int PORT = 6666;
+	/**
+	 * Nombre de personnages différents
+	 */
+	int NBPERSOS = 3;
+	/**
+	 * Caractère de séparation dans un chemin de fichiers
+	 */
+	String CHEMINSEPARATOR = "/";
+	/**
+	 * Chemin du dossier des images de fonds
+	 */
+	String CHEMINFONDS = "fonds"+CHEMINSEPARATOR;
+	/**
+	 * Chemin du dossier de l'image de la boule
+	 */
+	String CHEMINBOULES = "boules"+CHEMINSEPARATOR;
+	/**
+	 * Chemin du dossier de l'image du mur
+	 */
+	String CHEMINMURS = "murs"+CHEMINSEPARATOR;
+	/**
+	 * Chemin du dossier des images des personnages
+	 */
+	String CHEMINPERSONNAGES = "personnages"+CHEMINSEPARATOR;
+	/**
+	 * Chemin du dossier des sons
+	 */
+	String CHEMINSONS = "sons"+CHEMINSEPARATOR;
+	/**
+	 * Chemin de l'image de fond de la vue ChoixJoueur
+	 */
+	String FONDCHOIX = CHEMINFONDS+"fondchoix.jpg";
+	/**
+	 * Chemin de l'image de fond de la vue Arene
+	 */
+	String FONDARENE = CHEMINFONDS+"fondarene.jpg";
+	/**
+	 * Chemin du son welcome (entrée dans la vue ChoixJoueur)
+	 */
+	String SONWELCOME = CHEMINSONS+"welcome.wav";
+	/**
+	 * Chemin du son precedent (clic sur le bouton précédent de la vue ChoixJoueur)
+	 */
+	String SONPRECEDENT = CHEMINSONS+"precedent.wav";
+	/**
+	 * Chemin du son suivant (clic sur le bouton suivant de la vue ChoixJoueur)
+	 */
+	String SONSUIVANT = CHEMINSONS+"suivant.wav";
+	/**
+	 * Chemin du son go (clic sur le bouton GO de la vue ChoixJoueur)
+	 */
+	String SONGO = CHEMINSONS+"go.wav";
+	/**
+	 * Chemin du son flight (tir de la boule)
+	 */
+	String SONFIGHT = CHEMINSONS+"fight.wav";
+	/**
+	 * Chemin du son hurt (joueur blessé)
+	 */
+	String SONHURT = CHEMINSONS+"hurt.wav";
+	/**
+	 * Chemin du son death (joueur tué)
+	 */
+	String SONDEATH = CHEMINSONS+"death.wav";
+	/**
+	 * tableau des sons de la vue Arene
+	 */
+	String[] SON =  {CHEMINSONS + "fight.wav", CHEMINSONS + "hurt.wav", CHEMINSONS + "death.wav"} ;
+	/**
+	 * numéro correspondant au son FIGHT
+	 */
+	int FIGHT = 0;
+	/**
+	 * numéro correspondant au son HURT
+	 */
+	int HURT = 1;
+	/**
+	 * numéro correspondant au son DEATH
+	 */
+	int DEATH = 2;
+	/**
+	 * Extension des fichiers des images des personnages
+	 */
+	String EXTFICHIERPERSO = ".gif";
+	/**
+	 * Début du nom des images des personnages
+	 */
+	String PERSO = "perso";
+	/**
+	 * Chemin de l'image de la boule
+	 */
+	String BOULE = CHEMINBOULES+"boule.gif";
+	/**
+	 * Chemin de l'image du mur
+	 */
+	String MUR = CHEMINMURS+"mur.gif";
+	/**
+	 * état marche du personnage
+	 */
+	String MARCHE = "marche";
+	/**
+	 * état touché du personnage
+	 */
+	String TOUCHE = "touche";
+	/**
+	 * état mort du personnage
+	 */
+	String MORT = "mort";
+	/**
+	 * Caractère de séparation dans les chaines transférées
+	 */
+	String STRINGSEPARE = "~";
+	/**
+	 * Message "connexion" envoyé par la classe Connection
+	 */
+	String CONNEXION = "connexion";
+	/**
+	 * Message "reception" envoyé par la classe Connection
+	 */
+	String RECEPTION = "reception";
+	/**
+	 * Message "deconnexion" envoyé par la classe Connection
+	 */
+	String DECONNEXION = "deconnexion";
+	/**
+	 * Message "pseudo" envoyé pour la création d'un joueur
+	 */
+	String PSEUDO = "pseudo";
+	/**
+	 * vie de départ pour tous les joueurs
+	 */
+	int MAXVIE = 10 ;
+	/**
+	 * gain de points de vie lors d'une attaque
+	 */
+	int GAIN = 1 ; 
+	/**
+	 * perte de points de vie lors d'une attaque
+	 */
+	int PERTE = 2 ; 
+	/**
+	 * nombre de murs dans l'arène
+	 */
+	int NBMURS = 20;
+	/**
+	 * hauteur de la zone de jeu de l'arène
+	 */
+	int HAUTEURARENE = 600;
+	/**
+	 * largeur de la zone de heu de l'arène
+	 */
+	int LARGEURARENE = 800;
+	/**
+	 * hauteur d'un mur
+	 */
+	int HAUTEURMUR = 35;
+	/**
+	 * largeur d'un mur
+	 */
+	int LARGEURMUR = 34;
+	/**
+	 * hauteur du personnage
+	 */
+	int HAUTEURPERSO = 44;
+	/**
+	 * largeur du personnage
+	 */
+	int LARGEURPERSO = 39;
+	/**
+	 * hauteur du message
+	 */
+	int HAUTEURMESSAGE = 8;
+	/**
+	 * largeur de la boule
+	 */
+	int LARGEURBOULE = 17;
+	/**
+	 * hauteur de la boule
+	 */
+	int HAUTEURBOULE = 17;
+	/**
+	 * orientation du personnage vers la gauche
+	 */
+	int GAUCHE = 0;
+	/**
+	 * orientation du personnage vers la droite
+	 */
+	int DROITE = 1;
+	/**
+	 * taille du pas quand le personnage avance (nombre de pixels)
+	 */
+	int PAS = 10;
+	/**
+	 * nombre d'étapes (d'images) pour donner l'impresson de marche
+	 */
+	int NBETAPESMARCHE = 4;
+	/**
+	 * nombre d'étapes (d'images) pour donner l'impression d'être touché
+	 */
+	int NBETAPESTOUCHE = 2;
+	/**
+	 * nombre d'étapes (d'images) pour donner l'impresson de mourir
+	 */
+	int NBETAPESMORT = 2;
+	/**
+	 * Message "serveur" pour la création d'un serveur
+	 */
+	String SERVEUR = "serveur";
+	/**
+	 * Message "client" pour la création d'un client
+	 */
+	String CLIENT = "client";
+	/**
+	 * ordre pour ajouter un mur dans l'arène du serveur
+	 */
+	String AJOUTMUR = "ajout mur";
+	/**
+	 * ordre pour ajouter le panel des murs dans l'arène du client
+	 */
+	String AJOUTPANELMURS = "ajout panel murs";
+	/**
+	 * ordre pour ajouter un jLabel dans l'arène du serveur (joueur, message, boule)
+	 */
+	String AJOUTJLABELJEU = "ajout jLabel jeu";
+	/**
+	 * ordre pour modifier le panel du jeu dans l'aeène du client
+	 */
+	String MODIFPANELJEU = "modif panel jeu";
+	/**
+	 * ordre pour demander d'ajouter une phrase au tchat
+	 */
+	String TCHAT = "tchat";
+	/**
+	 * ordre pour ajouter une phrase dans l'arène du serveur
+	 */
+	String AJOUTPHRASE = "ajout phrase";
+	/**
+	 * ordre pour modifier le contenu du tchat dans l'arène du client
+	 */
+	String MODIFTCHAT = "modif tchat";
+	/**
+	 * ordre pour exécuter une action (déplacement, tire de boule)
+	 */
+	String ACTION = "action";
+	/**
+	 * ordre pour jouer un son (dans l'arène du client)
+	 */
+	String JOUESON = "joue son";
+
+}
